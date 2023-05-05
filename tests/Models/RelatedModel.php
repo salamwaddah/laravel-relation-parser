aa8aad392c398e5bf89f85a1dd5a0f153eb4928d
Salam <salamwaddah@gmail.com>
2023-05-05 19:10:43 +0400

    add test models

---
+++ tests/Models/RelatedModel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SalamWaddah\RelationParser\Tests\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RelatedModel extends Model
+{
+}
---
+++ tests/Models/TestModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SalamWaddah\RelationParser\Tests\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use SalamWaddah\RelationParser\LoadsRelations;
+
+class TestModel extends Model
+{
+    use LoadsRelations;
+
+    public function relatedModels(): HasMany
+    {
+        return $this->hasMany(RelatedModel::class);
+    }
+}
